1bd0f579a7b1e5ef46f5327a740fcaa017cb5d7e
Suvansh Choudhary <choudharysuvansh04@gmail.com>
2025-05-13 13:33:08 +0530

    f

--- app.py
+++ app.py
@@ -244,4 +244,5 @@ def estimate():
                            selected_wage=wage_type)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
